94e1ee1f378d4617803af8e56ca6fdddbe34c4ee
Srimathi.R <107202464+srimathirajarajan@users.noreply.github.com>
2024-03-13 00:23:53 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,32 +1,62 @@
+import mysql.connector
+import pandas as pd
 import os
 import streamlit as st
-from pathlib import Path
-from sql_execution import execute_mysql_query
-from PIL import Image
 from langchain import OpenAI, LLMChain
 from langchain.prompts import load_prompt
 
-# Setup env variable
-# os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY  # No need for this line anymore
+# Define function to execute MySQL query
+def execute_mysql_query(sql):
+    connection_params = {
+        'user': 'sql6690576',
+        'password': 'cmiie8J5uu',
+        'host': 'sql6.freesqldatabase.com',
+        'database': 'sql6690576',
+        'port': 3306,
+    }
 
-# Frontend
+    try:
+        print("Executing SQL query:", sql)
+        conn = mysql.connector.connect(**connection_params)
+        cur = conn.cursor()
+        cur.execute(sql)
+        query_results = cur.fetchall()
+        column_names = [col[0] for col in cur.description]
+        data_frame = pd.DataFrame(query_results, columns=column_names)
+        return data_frame
+
+    except mysql.connector.Error as e:
+        print("MySQL Error:", e)
+        return None
+
+    finally:
+        try:
+            cur.close()
+        except:
+            pass
+
+        try:
+            conn.close()
+        except:
+            pass
+
+# Streamlit setup
 st.title("AI SQL Assistant")
 user_input = st.text_input("Enter your query")
 tab_titles = ["Result", "Query"]
 tabs = st.tabs(tab_titles)
 
-# Pull OpenAI API key from Streamlit secrets
+# Load OpenAI API key from Streamlit secrets
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
-# Create the prompt
+# Load prompt template
 prompt_template = load_prompt('tpch_prompt.yaml')
 
-
-# Use the correct model name
+# Initialize OpenAI and SQL generation chain
 llm = OpenAI(model="gpt-4", temperature=0, api_key=OPENAI_API_KEY)
-
 sql_generation_chain = LLMChain(llm=llm, prompt=prompt_template, verbose=True)
 
+# Main functionality
 if user_input:
     sql_query = sql_generation_chain(user_input)
     if 'text' in sql_query:
@@ -41,7 +71,7 @@ if user_input:
                 if result is not None:
                     with tabs[0]:
                         st.write("Query Execution Result:")
-                        st.write(result)  # Display the query execution result
+                        st.write(result)
                 else:
                     with tabs[0]:
                         st.write("No result returned from the database.")
